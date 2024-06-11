476f5c10bd939e13dab650b0722a5a5deb26c487
Kevin Gallego <kevinelevengallego@gmail.com>
2024-06-10 22:19:35 -0500

    update archivos

--- app.js
+++ app.js
@@ -15,3 +15,27 @@ console.log('Cuarto console LOG');
 
 */
 
+//ASINCRONIA
+
+//COMO REALIZAR PETICIONES
+
+
+// COMO MANEJAR DATA
+
+const data = fetch ('https://jsonplaceholder.typicode.com/users');
+
+//console.log(data);
+
+data
+    .then((res) => {
+    return res.json();
+    //console.log(res);
+})
+
+.then((info) => {
+    info.forEach((user) =>{
+        console.log(user.name);
+    })
+    //console.log(info);
+});
+
